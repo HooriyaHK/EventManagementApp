3a820964dbc00ccdc4891ff66b996995d3b8cebd
Diego Arana <diegoarana@MacBook-Pro-158.local>
2024-10-19 18:36:54 -0600

    Revert "made changes to gradle for junit 5 usage, created unit tests for getters of user class, created admin home view, made changes to main activity so that it retrieves the data of the currently logged in user, have not tested yet"
    This reverts commit 250d75d00030cb6e788adad664df5146a793c6ea.

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -43,10 +43,6 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    // junit 5
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.1")
-
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
     implementation("com.google.firebase:firebase-analytics")
