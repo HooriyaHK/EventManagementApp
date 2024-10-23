222aa60dd0a5ac541f525937b0eb5752ed0a8add
jtuason8 <156242931+jctuason@users.noreply.github.com>
2024-10-23 10:04:24 -0600

    Us03.05.01 (#57)
    * Added Events and Waitlist models
    
    ---------
    
    Co-authored-by: jctuason <jctuason@ualberta.com>
    Co-authored-by: Diego Arana <106112103+zyzCodes@users.noreply.github.com>

--- code/app/src/main/java/com/example/goldencarrot/views/AdminHomeActivity.java
+++ code/app/src/main/java/com/example/goldencarrot/views/AdminHomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.goldencarrot.views;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +10,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.goldencarrot.R;
 
 public class AdminHomeActivity extends AppCompatActivity {
+    private Button viewAllEventsButton, viewAllUsersButton, viewAllImagesButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+
+        viewAllEventsButton = findViewById(R.id.adminAllEventsButton);
+        viewAllUsersButton = findViewById(R.id.adminAllUsersButton);
+        viewAllImagesButton = findViewById(R.id.adminAllImagesButton);
+
+        viewAllEventsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        viewAllUsersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        viewAllImagesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
